3cfeb58c9ae9812d4c1fe40c4b8b0e72809e457d
Richy <richardloeffler14@gmail.com>
2022-05-05 19:42:16 +0200

    CamScript follow player changes

--- Assets/Props/Scripts/Camscript.cs
+++ Assets/Props/Scripts/Camscript.cs
@@ -5,66 +5,67 @@ using UnityEngine;
 
 public class Camscript : MonoBehaviour
 {
-    public Camera[] Cameras;
+    public GameObject[] Cameras;
     public GameObject[] Figures;
     public bool Nextcam;
 
+    [SerializeField]
+    private GameMap _gameMapRef;
+
     public Vector3 CamOffset;
     public Vector3 CamRotate;
-
-    public int Figurenextindex = 0;
 
     // Start is called before the first frame update
     void Start() {
         Nextcam = false;
         foreach (var cam in Cameras) {
-            cam.enabled = false;
+            cam.SetActive(false);
         }
         //Only enable this camera
-        Cameras[0].enabled = true;
+        Cameras[0].SetActive(true);
 
-        CamOffset.x = -20;
-        CamOffset.y = -40;
-        CamOffset.z = 10;
+        CamOffset.x = 0;
+        CamOffset.y = 30;
+        CamOffset.z = -20;
 
         CamRotate.x = 40;
         CamRotate.y = 0;
         CamRotate.z = 0;
 
-        Cameras[0].transform.Rotate(CamRotate);
+        Cameras[0].transform.eulerAngles = CamRotate;
     }
 
     // Update is called once per frame
     void Update() {
         if (Input.GetKeyDown(KeyCode.C)) {
             foreach (var cam in Cameras) {
-                if (cam.enabled == false) {
+                if (cam.activeSelf == false) {
                     if (Nextcam == true) {
-                        cam.enabled = true;
+                        cam.SetActive(true);
                         Nextcam = false;
                     }
                     continue;
                 }
                 else {
-                    cam.enabled = false;
+                    cam.SetActive(false);
                     Nextcam = true;
                 }
             }
 
             if (Nextcam == true) {
-                Cameras[0].enabled = true;
+                Cameras[0].SetActive(true);
                 Nextcam = false;
             }
         }
 
-        if (Cameras[0].enabled == true) {
-            Cameras[0].transform.position = Figures[Figurenextindex].transform.position - CamOffset;
-        }
-
-        if (Input.GetKeyDown(KeyCode.P) && (Cameras[0].enabled == true)) {
-            Figurenextindex++;
-            if (Figurenextindex >= Figures.Length) {
-                Figurenextindex = 0;
+        if (Cameras[0].activeSelf == true) {
+            if ((_gameMapRef.isPlaying >= 0) && (_gameMapRef.isPlaying < Figures.Length)) {
+                Cameras[0].transform.position = Figures[_gameMapRef.isPlaying].transform.position + CamOffset;
+                Cameras[0].transform.eulerAngles = CamRotate;
+            }
+            else {
+                Cameras[0].transform.position = new Vector3(-75, 15, 45);
+                Cameras[0].transform.eulerAngles = new Vector3(15, -90, 0);
             }
         }
     }
