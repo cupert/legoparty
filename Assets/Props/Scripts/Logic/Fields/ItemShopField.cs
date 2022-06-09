9f0f991c3ca6a2c1a24c836d54cad04bb688fae9
TheEarlOfTea <julian.weghaupt@gmail.com>
2022-06-09 14:42:39 +0200

    Added dummy shop

--- Assets/Props/Scripts/Logic/Fields/ItemShopField.cs
+++ Assets/Props/Scripts/Logic/Fields/ItemShopField.cs
@@ -7,11 +7,31 @@ public class ItemShopField : MonoBehaviour, IField
     public FieldTypeEnum FieldType { get; set; } = FieldTypeEnum.ItemShopField;
     public bool IsDoneShopping { get; set; } = false;
     public bool PlayerInShop { get; set; } = false;
+    private int PlayerMoney=5;
+    private PlayerInfo playerInfo;
     public void InteractWithPlayer(PlayerInfo player)
     {
         PlayerInShop = true;
-        Debug.Log("shop not implemented yet");
-        // Debug.Log("press w if you are done shopping");
+        Debug.Log("press w if you are done shopping");
+        LoadShop();
+    }
+
+    void LoadShop(){
+        Debug.Log("Welcome to the shop");
+        GetShopItems();
+        Debug.Log("Press 1 for Double Dice, Press 2 for Custom Dice");
+    }
+    void GetShopItems(){
+        Debug.Log("Fetching Shop items!");
+        //return list of shop items
+    }
+    bool CheckMoney(int money){
+        Debug.Log("Coins available: " + PlayerMoney);
+        if(PlayerMoney<money){
+            return false;
+        }
+        PlayerMoney-=money;
+        return true;
     }
 
     // Start is called before the first frame update
@@ -23,10 +43,29 @@ public class ItemShopField : MonoBehaviour, IField
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown("w") && PlayerInShop)
-        {
-            PlayerInShop = false;
-            IsDoneShopping = true;
+        if(PlayerInShop){
+            if (Input.GetKeyDown("w")){
+                PlayerInShop = false;
+                IsDoneShopping = true;
+                return;
+            }
+            if (Input.GetKeyDown("1")){
+                if(CheckMoney(5)){
+                    Debug.Log("Player bought Double Dice");
+                    return;
+                }
+                Debug.Log("Player has not enough money to buy a Double Dice");
+                return;
+            }
+            if (Input.GetKeyDown("2")){
+                if(CheckMoney(10)){
+                    Debug.Log("Player bought Custom Dice");
+                    return;
+                }
+                Debug.Log("Player has not enough money to buy a Custom Dice");
+                return;
+            }
         }
+        
     }
 }
